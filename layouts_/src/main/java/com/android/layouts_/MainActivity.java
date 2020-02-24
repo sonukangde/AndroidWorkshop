2ddefc2b719072e175730128f01e6035b509e4bb
sonu <sonukangde@gmail.com>
2020-02-24 10:55:40 +0530

    layout

---
+++ layouts_/src/main/java/com/android/layouts_/MainActivity.java
@@ -0,0 +1,17 @@
+package com.android.layouts_;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editText2, editText3;
+    
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
