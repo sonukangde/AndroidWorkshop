b3ff7b72fd3cf86c39c72c4013b5424347178551
sonu <sonukangde@gmail.com>
2020-02-25 10:36:40 +0530

    LoginActivity

--- layouts_/src/main/java/com/android/layouts_/LoginActivity.java
+++ layouts_/src/main/java/com/android/layouts_/LoginActivity.java
@@ -4,11 +4,41 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 public class LoginActivity extends AppCompatActivity {
+
+    EditText edtUsername,edtPassword;
+    TextView signUp;
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        edtUsername=findViewById(R.id.edtUsername);
+        edtPassword=findViewById(R.id.edtPassword);
+        signUp=findViewById(R.id.txtSignUp);
+        btnLogin=findViewById(R.id.btnLogIn);
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Bundle bundle=new Bundle();
+                bundle.putString("callFrom","login");
+
+                Intent intent=new Intent(LoginActivity.this,MainActivity.class);
+                intent.putExtras(bundle);
+                startActivity(intent);
+            }
+        });
+
     }
 }
